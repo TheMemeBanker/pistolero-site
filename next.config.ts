49170ff14eaeb3069ccd7cc55612b529b34eed67
TheMemeBanker <thememebanker@users.noreply.github.com>
2026-07-26 10:19:39 -0500

    attach custom domain pistolero.casa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", basePath: "/pistolero-site", assetPrefix: "/pistolero-site/", images: { unoptimized: true }, trailingSlash: true,
+  output: "export", images: { unoptimized: true }, trailingSlash: true,
   /* config options here */
 };
 
